1f3dae5efcf1bc344dc0399065aefaaffa2d80b8
Utilit2010 <96230895+Harco2010@users.noreply.github.com>
2023-04-15 23:24:28 +0300

    1 2 3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,17 +2,17 @@
    
 </script>
 
-<main>
-  <div class="flex flex-col h-screen bg-gray-50">
-    <header class="h-10 w-full bg-gray-100 overflow-auto shadow-md">
+
+<div class="flex flex-col h-screen bg-gray-50">
+  <header class="h-10 w-full bg-gray-100 overflow-auto shadow-md">
         <div class="flex content-center items-center justify-end">
            <b class="flex justify-end text-center" />
         </div>
-    </header>
-    <nav class="flex">
-        <input id="menu__toggle" type="checkbox" />
+  </header>
+  <nav class="flex">
+        <input class="w-0 h-0" id="menu__toggle" type="checkbox" />
         <label class="menu__btn" for="menu__toggle">
-          <span />
+          <span></span>
         </label>
         <ul class="menu__box">      
           <li>
@@ -25,17 +25,19 @@
             <a class="menu__item" href="/connection">{"Подключение"}</a>
           </li>
         </ul>
-        <ul class="menu__main ml-0">
-           <div class="bg-cover pt-8 px-4">
-              <div class="crd-grd-ln">
-                <div class="crd">
-                   <h1 class="crd-hdr">flprog_svelte</h1>
-                </div> 
-              </div>
-           </div>
-        </ul>
-    </nav>
-  </div>  
-</main>
+  </nav>  
+  <main class="flex-1 overflow-y-auto p-0 ml-0">
+    <ul class="menu__main">
+      <div class="bg-cover pt-0 px-4">
+        <div class="grd-1col1 animate-pulse"></div>
+        <div class="grd-3col1">
+          <div class="crd">
+            <h1 class="crd-hdr">flprog_svelte</h1>
+          </div>
+        </div>
+      </div>
+    </ul>
+  </main>
+</div>
 
 
